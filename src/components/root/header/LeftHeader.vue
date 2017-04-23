9e02f3442b94943082f29604c6653aaa73c812d2
Diego Ferreira <diego.diegoferreira@gmail.com>
2017-04-23 10:58:07 -0300

    ajuste menu-left para exibir os menus apenas se usuario logado

--- src/components/root/header/LeftHeader.vue
+++ src/components/root/header/LeftHeader.vue
@@ -1,13 +1,17 @@
 <template>
-  <ul class="nav navbar-nav">
-    <router-link to="/dashboard" tag="li" exact><a>Campeonatos</a></router-link>
+  <ul class="nav navbar-nav" v-if="isLogged()">
+    <router-link to="/dashboard" tag="li" exact ><a>Campeonatos</a></router-link>
     <!-- <li class="active"><a href="#">Campeonatos <span class="sr-onlcurrent)</span></a></li> -->
     <li><a href="#">Usuários  </a></li>
   </ul>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 export default {
-  name: 'LeftHeader'
+  name: 'LeftHeader',
+  methods: {
+    ...mapGetters(['isLogged'])
+  }
 }
 </script>
